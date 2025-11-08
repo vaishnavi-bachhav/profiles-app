8a1ff0bc8b35f0ae0fca3648aa2a09a313b827ce
Vaishnavi Bachhav <vaishnavi.bachhav111@gmail.com>
2025-11-08 00:13:38 +0000

    Enhancement - Added modal popup for add profile

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from "react-bootstrap/Button";
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 import ProfileCard from './components/ProfileCard.jsx';
 import { profiles } from './data/profiles.js';
 import { useState } from 'react';
-import Form from 'react-bootstrap/Form';
 
 export default function App() {
   const [people, setPeople] = useState(profiles);
   const [name, setName] = useState('');
   const [errors, setErrors] = useState({});
-  const [validated, setValidated] = useState(false);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-   // Increment likes for a profile
+  // Increment likes for a profile
   const likePeople = (id) => {
     setPeople(ps => ps.map(p => p.id === id ? { ...p, likes: p.likes + 1 } : p));
   }
@@ -35,11 +38,10 @@ export default function App() {
   }
 
   const handleSubmit = (e) => {
-        e.preventDefault();
+    e.preventDefault();
 
     const form = e.target;
     const formIsValid = validateForm(form);
-    setValidated(true);
     if (formIsValid) {
       const newProfile = {
         id: people.length ? Math.max(...people.map(p => p.id)) + 1 : 1,
@@ -49,22 +51,41 @@ export default function App() {
       setPeople(ps => [...ps, newProfile]);
       setName('');
       setErrors({});
-      setValidated(false);
       form.reset();
+      handleClose();
     }
   }
 
   return (
     <Container className="py-4">
       <h1 className="mb-4 text-center">Profiles</h1>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control name='name' value={name} type="text" placeholder="Enter your name" onChange={(e) => setName(e.currentTarget.value)} isInvalid={!!errors.name} />
-          <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
-        </Form.Group>
-        <Button variant="primary" type='submit' className="my-2">Add Profile</Button>
-      </Form>
+      <Button variant="primary" className='' onClick={handleShow}>
+        Add Profile
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Form noValidate onSubmit={handleSubmit}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add Profile</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Fill out the form below to add a new profile.</p>
+            <Form.Group className="mb-3" controlId="formName">
+              <Form.Label className='fw-bold'>Name</Form.Label>
+              <Form.Control name='name' value={name} type="text" placeholder="Enter your name" onChange={(e) => setName(e.currentTarget.value)} isInvalid={!!errors.name} />
+              <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type='submit'>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+
       <hr />
       <Row xs={1} md={2} lg={3} className="g-3">
         {people.map(p => (
